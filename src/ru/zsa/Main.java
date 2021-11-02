cc1684eb1331d91f9d620439124e561c2504e84a
PaulFrere <paulfrere91@gmail.com>
2021-11-02 16:24:56 +0700

    Finish

--- src/ru/zsa/Main.java
+++ src/ru/zsa/Main.java
@@ -3,5 +3,14 @@ package ru.zsa;
 public class Main {
 
     public static void main(String[] args) {
+
+        String firstName = "Sergey";
+        String lastName = "Zherebtsov";
+        int age = 35;
+        String hobby = "Formula-1";
+
+        System.out.println(firstName + " " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
     }
 }
